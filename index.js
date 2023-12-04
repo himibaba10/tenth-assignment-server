57a073fa4f5c013ddebb67d48b504e5c91e66716
Ferdous Ahmed Himel <himibaba10@gmail.com>
2023-12-04 19:02:34 +0600

    Created route for getting cart product based on email

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ async function run() {
 
     const brandCollection = client.db("brandDB").collection("brands");
     const productCollection = client.db("brandDB").collection("products");
+    const cartCollection = client.db("brandDB").collection("cart");
 
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -60,6 +61,31 @@ async function run() {
     app.post("/products", async (req, res) => {
       const addedProduct = req.body;
       const result = await productCollection.insertOne(addedProduct);
+      res.send(result);
+    });
+
+    // For cart
+    app.get("/:email", async (req, res) => {
+      const email = req.params.email;
+
+      const query = { email: email };
+      const cursor = cartCollection.find(query);
+      const result = await cursor.toArray();
+
+      res.send(result);
+    });
+
+    app.post("/cart", async (req, res) => {
+      const newProduct = req.body;
+      const result = await cartCollection.insertOne(newProduct);
+      res.send(result);
+    });
+
+    app.delete("/delete-product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+
       res.send(result);
     });
   } catch (err) {
